18992f9338629d18296be195217ea7d8a319d85a
thomas dubiel <td1729@hotmail.com>
2019-12-15 02:43:30 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,5 +4,8 @@ def my_all?(collection)
   i = 0 
   returned_values = []
   while i < collection.length 
-    returned_values << yield(collection[i])
+    returned_values << yield(collection[i]) 
+    i += 1 
+  end 
+  returned_values 
 end
